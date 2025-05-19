4fa3cc4ad0e65083cbc27f60e34ada87981d3b9d
Giorgi <giorgi.tughushi@leverx.com>
2025-05-19 14:12:35 +0400

    Create Spring Boot project backbone with pom.xml

---
+++ src/test/java/com/leverx/learning_management_system/LearningManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leverx.learning_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
