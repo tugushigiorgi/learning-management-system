baf12e7dacc0cb283e25822fb9e20e83181a9554
Giorgi <giorgi.tughushi@leverx.com>
2025-05-19 17:41:56 +0400

    feature/add-Student-Controller

---
+++ src/main/java/com/leverx/learning_management_system/student/StudentController.java
@@ -0,0 +1,58 @@
+package com.leverx.learning_management_system.student;
+
+import static com.leverx.learning_management_system.util.ControllerResponse.handleItemOrNotFound;
+import static com.leverx.learning_management_system.util.ControllerResponse.handleList;
+
+import com.leverx.learning_management_system.student.dto.CreateStudentDto;
+import com.leverx.learning_management_system.student.dto.StudentDto;
+import com.leverx.learning_management_system.student.dto.UpdateStudentDto;
+import com.leverx.learning_management_system.student.service.StudentService;
+import java.util.List;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/students")
+@RequiredArgsConstructor
+public class StudentController {
+
+  private final StudentService studentService;
+
+  @PostMapping
+  public ResponseEntity<Void> createStudent(@RequestBody CreateStudentDto studentDto) {
+    studentService.createStudent(studentDto);
+    return ResponseEntity.ok().build();
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<StudentDto> getStudent(@PathVariable UUID id) {
+    return handleItemOrNotFound(studentService.getStudentById(id));
+  }
+
+  @GetMapping
+  public ResponseEntity<List<StudentDto>> getAllStudents() {
+    return handleList(studentService.getAllStudents());
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Void> deleteStudent(@PathVariable UUID id) {
+    studentService.deleteById(id);
+    return ResponseEntity.noContent().build();
+  }
+
+  @PutMapping
+  public ResponseEntity<Void> updateStudent(@RequestBody UpdateStudentDto studentDto) {
+    studentService.updateStudent(studentDto);
+    return ResponseEntity.noContent().build();
+  }
+}
+
