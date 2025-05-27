3e676c8d28aad52d6569fa6a41ed942fc39f5674
Giorgi <giorgi.tughushi@leverx.com>
2025-05-27 16:35:32 +0400

    [STEP-1 & STEP-2 ] Configure manifest.yaml for application deployment and deploy

---
+++ src/main/java/com/leverx/learningmanagementsystem/MainController.java
@@ -0,0 +1,13 @@
+package com.leverx.learningmanagementsystem;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+  @GetMapping("/")
+  public String hello() {
+    return "Hello World!";
+  }
+}
